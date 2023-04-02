ed9026fed0a09f3079a513d88a5d57f3d3952228
pinkjagnie <olga.krzysztofik@gmail.com>
2023-04-03 00:04:02 +0200

    Added Navbar with styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,23 @@ import { Route, Routes } from "react-router-dom";
 
 import { Container } from "react-bootstrap";
 
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Store from "./pages/Store";
 import About from "./pages/About";
 
 const App: React.FC = () => {
   return (
-   <Container className="mb-4">
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/store" element={<Store />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
-   </Container>
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+   </>
   )
 }
 
