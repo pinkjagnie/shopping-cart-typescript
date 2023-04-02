4e8044a0bcbc11c61c2b5c20f03a150393e11bdb
pinkjagnie <olga.krzysztofik@gmail.com>
2023-04-03 00:30:00 +0200

    Setup Store page & add simple StoreItem comp

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import StoreItem from "../components/StoreItem";
+
+import storeItems from "../data/items.json";
+
+import { Row, Col } from "react-bootstrap";
 
 const Store: React.FC = () => {
   return(
-    <h1>Store</h1>
+    <Row md={2} xs={1} lg={3} className="g-3">
+      {storeItems.map(item => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+    </Row>
   )
 };
 
