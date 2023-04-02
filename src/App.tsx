c48e0d1d9a998bce34e262950b53cececd24ebae
pinkjagnie <olga.krzysztofik@gmail.com>
2023-04-02 23:49:53 +0200

    Setup of routing & core pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from "react"
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import { Container } from "react-bootstrap";
+
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
 
 const App: React.FC = () => {
   return (
-   <h1>Hello</h1>
+   <Container className="mb-4">
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/store" element={<Store />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+   </Container>
   )
 }
 
---
+++ src/pages/Store.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Store: React.FC = () => {
+  return(
+    <h1>Store</h1>
+  )
+};
+
+export default Store;
